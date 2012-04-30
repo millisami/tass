905daa785caa7d075db7a259b503580c33162fe4
MillisamiSprout <millisami@gmail.com>
2012-04-30 17:16:06 +0545

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tass::Application.config.secret_token = 'd67b8b0dc38aef056c95137f40013b2d6c5a740446f5b7bbdbbdb1023b5a7e24b4e9a3c70784cb900281f739c36aca4b6794765dd285f68522a0bc2b9f5b7a20'
